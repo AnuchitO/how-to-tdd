a02d7c53cb58e40bcb093d4c281720c9eae849f2
AnuchitO <anuchit.prasertasng@gmail.com>
2021-10-07 14:55:07 +0700

    6: write a failing test for introduce parameter to function fizzbuzz

--- js/src/fizzbuzz.test.js
+++ js/src/fizzbuzz.test.js
@@ -2,13 +2,17 @@ const { fizzbuzz } = require("./fizzbuzz")
 
 describe("FizzBuzz", () => {
   it("should return '1' when input 1", () => {
-    // Arrange
     const input = 1
 
-    // Act
     const result = fizzbuzz(input)
 
-    // Assertion
     expect(result).toBe("1")
+  })
+  it("should return '2' when input 2", () => {
+    const input = 2
+
+    const result = fizzbuzz(input)
+
+    expect(result).toBe("2")
   })
 })
